1eab6d831510eef25a7076e4d87a9f1df1eeaa80
Hugo <rytlewski.hugo@etu.educ-valadon-limoges.fr>
2023-09-04 15:50:22 +0200

    ddd

--- components/Layouts/Footer.vue
+++ components/Layouts/Footer.vue
@@ -1,7 +1,7 @@
 <template> 
     <footer class="bg-neutral-900 xl:w-[80rem] mt-30 translate-y-24 rounded-xl m-4 ">
         <div class="w-screen-xl items-center p-4 flex flex-col xl:flex-row xl:justify-between">
-            <span class="text-xl text-white sm:text-center ">© 2023 
+            <span class="text-lg text-white sm:text-center ">© 2023 
                 <a class="">Météo Hugo, Tous droits réservés</a>
         
             </span>
